c21508d7f6f15d3d32a0c9d3bf8c50aabb8ed0f0
Amanda Balderas M <Amanda29@users.noreply.github.com>
2018-10-04 20:25:29 -0500

    actualizando calculo ingreso

--- censos/base_censos_0410.r
+++ censos/base_censos_0410.r
@@ -115,7 +115,6 @@ x_vivienda <- x_vivienda[, colnames(x_vivienda) != "(Intercept)"]
 
 # extraer parámetros obtenidos con el modelo
 load("fit.RData")
-#load("I:/censos/prueba_fit.RData")
 ext_fit <- extract(fit)
 beta_mun <- ext_fit$beta_mun
 beta_0 <- ext_fit$beta_0
@@ -125,27 +124,13 @@ rm('ext_fit', 'fit')
 
 # función para estimar el ingreso
 gen_quantities_r <- function(x_vivienda2, municipio){
-  n_sims <- length(x_vivienda2)
   reg_pred <- beta_0 + apply(x_vivienda2 * beta_pos, 1, sum) + beta_mun[,municipio]
-  log_ing <- apply(reg_pred, 1, function(x) rnorm(n_sims, mean = x, sd = sigma_pos))
+  log_ing <- apply(reg_pred, 1, function(x) rnorm(nrow(x_vivienda2), mean = x, sd = sigma_pos))
   return(log_ing)
 }
-
-# función para estimar el ingreso
-gen_quantities_r <- function(x_vivienda2, municipio){
-  reg_pred <- beta_0 + apply(x_vivienda2 * beta_pos, 1, sum) + beta_mun[,municipio]
-  log_ing <- rep(0, nrow(x_vivienda2))
-  for(i in 1:nrow(x_vivienda2)){
-    log_ing[i] <- rnorm(1, reg_pred[i], sigma_pos[i])
-  }
-  return(log_ing)
-}
-
 
 # estimacion del ingreso por municipio
 level_tam_loc <- unique(datos_limpios_vivienda$tam_loc)
-level_tenen <- unique(datos_limpios_vivienda$tenen)
-level_combus <- unique(datos_limpios_vivienda$combus)
 municipios <- unique(datos_limpios_vivienda$geo_id)
 log_ing_mun_final <- NULL
 
@@ -153,42 +138,14 @@ for(i in 1:length(municipios)){
   municipio <- municipios[i]
   datos_limpios_vivienda1 <- datos_limpios_vivienda %>%
     filter(geo_id == municipio)
-  #x_vivienda1 <- model.matrix(~ jefe_sexo + pisos + dis_agua + excus + 
-                    #drenaje + servicio_celular + servicio_internet + automovil + tam_hog +
-                    #n_ocup + max_ed + n_ocup * max_ed + factor(tam_loc, levels=level_tam_loc) + indigena, 
-                    #data = datos_limpios_vivienda1)
-  x_vivienda1 <- model.matrix(~ - 1 + factor(tenen, , levels=level_tenen) + drenaje_1 + elect_1 + 
-                                factor(combus, levels=level_combus) + eli_ba_1 + cua_coc_1 + 
-                                excus_1 + vehi_1 + serv_comu_n + 
-                                act_mayvalor_n + act_normal_n + equipa_n + 
-                                cuart_n + tam_hog_n + hab_cuar_prom + 
-                                hombres_n + mujeres_n + menores_n + 
-                                p12_64_n + p65mas_n + ocup_n + 
-                                pering_n + donativo_1 + remesas_1 + 
-                                bene_gob_1 + j_sexo + j_edad + 
-                                j_ed_formal + max_nved_h + hablaind_n + 
-                                alfabe_n + ic_asalud_n + rezedu_may16_n + 
-                                ins_ali_h + ic_cv_h + ic_sbv_h + 
-                                rel_depen + tasa_mort + j_ocup + 
-                                j_rezedu + j_hablaind + j_disc + 
-                                j_ind + j_sub + j_sermed + 
-                                j_agui + j_vac + j_utili + 
-                                j_afore + j_sect + cony_sexo + 
-                                cony_edad + cony_ed_formal + cony_ocup + 
-                                cony_rezedu + cony_hablaind + cony_disc + 
-                                cony_ind + cony_sub + cony_sermed + 
-                                cony_agui + cony_vac + cony_utili + 
-                                cony_afore + cony_sect + p16_menos_trab_n + 
-                                p16_64_trab_n + p65mas_trab_n + jub_pen_n + 
-                                factor(tam_loc, levels=level_tam_loc), 
-                              data = datos_limpios_vivienda1)
-  #x_vivienda1 <- x_vivienda1[, colnames(x_vivienda1) != "(Intercept)"]
-  pred_log_ing_mun <- rep(0, 1500)
+  x_vivienda1 <- model.matrix(~ jefe_sexo + pisos + dis_agua + excus + 
+                    drenaje + servicio_celular + servicio_internet + automovil + tam_hog +
+                    n_ocup + max_ed + n_ocup * max_ed + factor(tam_loc, levels=level_tam_loc) + indigena, 
+                    data = datos_limpios_vivienda1)
+  x_vivienda1 <- x_vivienda1[, colnames(x_vivienda1) != "(Intercept)"]
+  pred_log_ing_mun <- rep(0,  nrow(beta_0))
   for(j in 1:nrow(x_vivienda1)){
-    x_vivienda2 <- NULL
-    for(k in 1:1500){
-      x_vivienda2 <- rbind(x_vivienda2, x_vivienda1[j,])
-    }
+    x_vivienda2 <- matrix(x_vivienda1[j,], nrow = nrow(x_vivienda1), ncol =  nrow(beta_0), byrow = TRUE) 
     pred_log_ing <- gen_quantities_r(x_vivienda2, municipio)
     pred_log_ing_mun <- pred_log_ing_mun + pred_log_ing
   }
@@ -197,26 +154,3 @@ for(i in 1:length(municipios)){
 }
 
 write_csv(x = log_ing_mun_final, path = 'resultados_mun.csv')
-
-#################################################################################################
-
-prueba <- x_vivienda2 * beta_pos
-prueba1 <- apply(prueba, 1, sum)
-prueba2 <- beta_0 + prueba1
-reg_pred <- prueba2 + beta_mun[,1]
-muni <- datos_limpios_vivienda$geo_id[1]
-reg_pred <- beta_0 + apply(x_vivienda2 * beta_pos, 1, sum) + beta_mun[,muni]
-for(i in 1:nrow(x_vivienda2)){
-  log_ing[i] <- rnorm(1, reg_pred[i], sigma_pos[i])
-}
-
-x_vivienda2 <- NULL
-for(j in 1:1500){
-  x_vivienda2 <- rbind(x_vivienda2, x_vivienda1[1,])}
-
-###########################################################################################
-
-tra_pob <- read.dbf('C:/Users/H11765/Downloads/Tra_Poblacion_2010_concil_2010_dbf/poblacion.dbf')
-tra_hog <- read.dbf('C:/Users/H11765/Downloads/Tra_Hogares_2010_concil_2010_dbf/Tra_Hogares_2010_concil_2010_DBF.dbf')
-nueva_pob <- read.dbf('C:/Users/H11765/Downloads/NCV_Poblacion_2010_concil_2010_dbf/NCV_Poblacion_2010_concil_2010_DBF.dbf')
-nueva_hog <- read.dbf('C:/Users/H11765/Downloads/NCV_Hogares_2010_concil_2010_dbf/NCV_Hogares_2010_concil_2010_DBF.dbf')
